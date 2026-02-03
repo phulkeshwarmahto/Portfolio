69517070a272fc92e8860860f1a2a24e55430589
Phulkeshwar Mahto <phulkeshwarmahto9@gmail.com>
2026-02-03 19:15:49 +0530

    D

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE, // e.g., 'gmail'
+        service: process.env.EMAIL_SERVICE,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
